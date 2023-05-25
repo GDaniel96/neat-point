ca425e0dc5cdd07842234085bbc8ea77be434202
GDaniel96 <george.daniel1996@yahoo.com>
2023-05-25 22:39:33 +0300

    implement contact component for both mobile and desktop and used emailjs for form submitting - pending verification of the environment variables in the next.config.js file

---
+++ src/components/Contact/index.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import Button from "../common/Button";
+import BlockHeader from "../common/BlockHeader";
+import BlockParagraph from "../common/BlockParagraph";
+import BlockContent from "../common/BlockContent";
+import { Row, Col, Container } from "reactstrap";
+import screens from "../Layout/mediaQueries";
+import Form from "./Form";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media only screen and (${screens.md}) {
+    width: 515px;
+  }
+`;
+
+const Contact = () => {
+  return (
+    <Container>
+      <BlockContent>
+        <Row>
+          <Col lg={2} />
+          <Col lg={8}>
+            <Row>
+              <Wrapper>
+                <TextWrapper>
+                  <BlockHeader>
+                    Scrie-ne un mesaj pentru a primi oferta adaptă nevoilor tale
+                  </BlockHeader>
+                  <BlockParagraph>
+                    Serviciile noastre se aplică doar în Județul Brașov
+                  </BlockParagraph>
+                </TextWrapper>
+              </Wrapper>
+            </Row>
+            <Row>
+              <Form />
+            </Row>
+          </Col>
+          <Col lg={2} />
+        </Row>
+      </BlockContent>
+    </Container>
+  );
+};
+
+export default Contact;
--- src/components/common/BlockContent.js
+++ src/components/common/BlockContent.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const BlockContent = styled.div`
-  padding-top: 55px;
+  padding-top: 50px;
 `;
 
 export default BlockContent;
